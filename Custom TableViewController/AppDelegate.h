46c4123fadb1104416c60ef1c9d9c91c62d4e682
trucsphuongw <osxvn@trucphuong.local>
2018-02-07 10:55:07 +0700

    Initial Commit

---
+++ Custom TableViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Custom TableViewController
+//
+//  Created by Trúc Phương >_< on 07/02/2018.
+//  Copyright © 2018 iDev Bao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
